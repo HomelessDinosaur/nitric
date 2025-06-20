ed72741e88175fed227c402170fe2be97dbae9ac
Ryan Cartwright <fryzee.cartwright@gmail.com>
2025-06-20 13:24:07 +1000

    test default to PORT

--- server/runtime/start.go
+++ server/runtime/start.go
@@ -73,7 +73,7 @@ func Start(cmd string) {
 	runCmd.Stdout = os.Stdout
 	runCmd.Stderr = os.Stderr
 
-	servicePort := os.Getenv("NITRIC_GUEST_PORT")
+	servicePort := os.Getenv("PORT")
 	if servicePort == "" {
 		servicePort = os.Getenv("PORT")
 	} else {
